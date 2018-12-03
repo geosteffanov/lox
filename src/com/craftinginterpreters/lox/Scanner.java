aac152c7e5761cc739d58f32ce0aaa8d4bfa8a57
Georgi Stefanov <georgi.stefanov@uber.com>
2018-12-03 02:36:35 +0200

    Add scanning of two symbol lexemes

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -3,6 +3,8 @@ package com.craftinginterpreters.lox;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.craftinginterpreters.lox.TokenType.*;
+
 
 public class Scanner {
     private final String source;
@@ -35,7 +37,56 @@ public class Scanner {
         return current >= source.length();
     }
 
-    
+
+
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG);break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL);break;
+            case '<': addToken(match('=') ? LESS_EQUAL: LESS);break;
+            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER);break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
+
+        return true;
+    }
+
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+
 
 
 
