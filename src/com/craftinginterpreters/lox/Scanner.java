f31d3e582ad8eb25e41af5fcec89dff2cddbfb24
Georgi Stefanov <georgi.stefanov@uber.com>
2018-12-07 15:31:52 +0200

    Add Scanner complete code

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,6 @@
 package com.craftinginterpreters.lox;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -9,6 +8,27 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<Token>();
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("or", OR);
+        keywords.put("true", TRUE);
+        keywords.put("false", FALSE);
+        keywords.put("if", IF);
+        keywords.put("else", ELSE);
+        keywords.put("for", FOR);
+        keywords.put("while", WHILE);
+        keywords.put("fun", FUN);
+        keywords.put("return", RETURN);
+        keywords.put("class", CLASS);
+        keywords.put("this", THIS);
+        keywords.put("super", SUPER);
+        keywords.put("nil", NIL);
+        keywords.put("print", PRINT);
+        keywords.put("var",VAR);
+    }
 
     private int start = 0;
     private int current = 0;
@@ -57,10 +77,41 @@ public class Scanner {
             case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL);break;
             case '<': addToken(match('=') ? LESS_EQUAL: LESS);break;
             case '>': addToken(match('=') ? GREATER_EQUAL : GREATER);break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                } break;
+            case ' ': break;
+            case '\r': break;
+            case '\n': line++; break;
+            case '\t': break;
+            case '"': string(); break;
 
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character: '" + c + "'.");
+                }
+                break;
         }
+    }
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z')||
+                (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return isDigit(c) || isAlpha(c);
     }
 
     private boolean match(char expected) {
@@ -71,10 +122,19 @@ public class Scanner {
         return true;
     }
 
-
     private char advance() {
         current++;
         return source.charAt(current - 1);
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
     }
 
     private void addToken(TokenType type) {
@@ -87,7 +147,42 @@ public class Scanner {
     }
 
 
+    private void number() {
+        while(isDigit(peek())) advance();
 
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
 
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
 
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current  - 1 );
+        addToken(STRING, value);
+    }
+
+    private void identifier() {
+        while(isAlpha(peek())) advance();
+
+        String lexeme = source.substring(start, current);
+
+        TokenType type = keywords.get(lexeme);
+
+        if (type == null) type = IDENTIFIER;
+
+        addToken(type, source.substring(start, current));
+    }
 }
